bec24f8f4dab585623698bf2f3c189be820e9f7e
Kaamii <kami.yh@medialand.tw>
2023-11-01 15:34:26 +0800

    Updates

--- vue.config.js
+++
@@ -1,6 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "/R2B1/" : "/",
-  transpileDependencies: true,
-  outputDir: 'dist',
-})
