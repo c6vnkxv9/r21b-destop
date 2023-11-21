d8483e53f495f71a456ea78c16ef7f26dd99d2cb
personal <k01midy@gmail.com>
2023-11-21 23:45:14 +0800

    feat:角色建立

--- src/components/GameSetting.vue
+++ src/components/GameSetting.vue
@@ -7,53 +7,107 @@
     .setting-wrap.mx-1280
         h1.title-style.text-start.mb-6 創建遊戲
         .d-flex
-            div
-                p.d-inline-block 卡片牌局：
-                BFormSelect(v-model="selected" :options="options")
+            .d-flex
+                p 卡片牌局：
+                BFormSelect(v-model="selectedModeId" :options="roleOptions")
                 
             div
+                i( class="bi bi-person-badge")
                 p.d-inline-block 人數：
-        p {{ selectedList }}
-
+        BFormGroup(v-slot="{ ariaDescribedby }")
+            BFormCheckbox.d-block(
+            v-for="pair in filteredRoles"
+            v-model="selectedPairId"
+            :key="pair.pair"
+            :value="pair.pair"
+            :aria-describedby="ariaDescribedby"
+            name="flavour-3a")
+                .d-flex.justify-content-between
+                    p(v-for="person in pair.roles" )
+                        i( class="bi bi-person-badge icon-color" :style='SetTeamColor(person.color)') 
+                        | {{ person.label }}
 </template>
 <script lang="ts">
 import 'bootstrap-icons/font/bootstrap-icons.css'
-import { BFormSelect, BFormCheckboxGroup } from 'bootstrap-vue-3'
+import { BFormSelect, BFormCheckboxGroup,BFormCheckbox,BFormGroup } from 'bootstrap-vue-3'
 import { ref, Ref,watch, defineComponent } from 'vue'
 import charactersData from '@/assets/data/characters.json'
 import colorList from '@/assets/data/colorList.json'
 import script from '@/assets/data/script.json'
-interface Option {
+interface RoleOptions {
     value: number;
     text: string;
 }
-
+interface Role{
+    key: string;
+    role: string;
+    color: string;
+    label: string;
+    desc: string;
+    pair: number;
+    src: string;
+}
+interface GroupedRoles{
+    pair: Number;
+    roles: Role[]
+}
 export default defineComponent({
+    components: {
+        BFormSelect, BFormCheckboxGroup,BFormCheckbox,BFormGroup
+  },
     setup() {
-        const options: Option[] = script.map(x => ({ value: x.key, text: x.label }));
-        const selected = ref<number>(options[0].value);
-        const selectedList = ref<string[]>([]);
+        const roleOptions: RoleOptions[] = script.map(x => ({ value: x.key, text: x.label }));
+        const selectedModeId = ref<number>(roleOptions[0].value);
+        const selectedPairId = ref<string[]>([]);
+        const filteredRoles=ref<GroupedRoles[]>([])
 
-        watch(selected, (newValue) => {
-            concatRoleList(newValue)
+        watch(selectedModeId, (newValue) => {
+            const list=getRelatedRoleKeys(newValue)
+            const roles=filterRolesByKeys(list)
+            filteredRoles.value=pairRoleList(roles)
 
         }, { immediate: true });
-        function concatRoleList(newValue:number){
+        function getRelatedRoleKeys(newValue:number){
+            let list:string[]=[]
             const item = script.find(s => s.key === newValue);
             if (item) {
-                selectedList.value = item.required.concat(item.options);
+                list = item.required.concat(item.options);
             }
+            return list
         }
-        function pickRoleList(){
-            charactersData
+        function filterRolesByKeys(selectedList:string[]){
+            const list =charactersData.filter(x=>selectedList.includes(x.key))
+            return list
+        }
+        function pairRoleList(list:Role[]){
+            return list.reduce((acc,arr)=>{
+                const item=acc.find(x=>x.pair==arr.pair)
+                if(item){
+                    item.roles.push(arr)
+                }else{
+                    acc.push({pair:arr.pair,roles:[arr]})
+                }
+                return acc
+            },[]as GroupedRoles[])
+        }
+        function SetTeamColor(color:string){
+            let colorHex=colorList.find(x=>x.label==color)?.color
+            return {'--color':colorHex}
         }
         return {
-            options, selected, selectedList
+            roleOptions, selectedModeId, filteredRoles,selectedPairId,SetTeamColor
         };
     }
 });
 </script>
 <style lang="scss" scoped>
+.icon{
+    font-size: 20px;
+    color: #000;
+}
+.icon-color{
+    color: var(--color);
+}
 .banner-wrap {
     background-color: #4a1212;
     padding: 20px 0;
