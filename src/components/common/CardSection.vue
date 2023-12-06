11bbf832438d3aac53693778a51c8ce78dd752e1
Kaamii <kami.yh@medialand.tw>
2023-12-06 19:11:05 +0800

    feeeeat:group:CardSection

--- src/components/common/CardSection.vue
+++ src/components/common/CardSection.vue
@@ -22,10 +22,12 @@ interface CardSection {
     team: string;
     color: string;
     roles: Role[];
-    singleRoles: Role[];
-    pairRoles: Role[][];
+    groupedRoles: GroupedRoles;
 }
-
+interface GroupedRoles {
+    single: Role[];
+    pair: Role[]
+}
 export default defineComponent({
     name: 'CardSection',
     props: {
@@ -40,7 +42,13 @@ export default defineComponent({
     },
     setup(props) {
         const RoleArray = computed(() => processRoles(props.data));
+        const singleRoles = computed(() => {
+            return RoleArray.value.flatMap((group:CardSection)=> group.groupedRoles.single);
+        });
 
+        const pairRoles = computed(() => {
+            return RoleArray.value.flatMap((group:CardSection) => group.groupedRoles.pair);
+        });
         function processRoles(data: Role[]): CardSection[] {
             let groupedData = groupByTeam(data);
             return groupedData.map(group => categorizeRoles(group));
@@ -54,8 +62,7 @@ export default defineComponent({
                         team: role.color,
                         color: setBGC(role.color),
                         roles: [],
-                        singleRoles: [],
-                        pairRoles: []
+                        groupedRoles: { single: [], pair: [] }
                     };
                     acc.push(team);
                 }
@@ -68,38 +75,39 @@ export default defineComponent({
             const counts = _.countBy(group.roles, 'pair');
             _.forEach(counts, (count, key) => {
                 if (count === 1) {
-                    group.singleRoles.push(...group.roles.filter(role => role.pair === +key));
+                    group.groupedRoles.single.push(...group.roles.filter(role => role.pair === +key));
                 } else if (count > 1) {
-                    group.pairRoles.push(group.roles.filter(role => role.pair === +key));
+                    group.groupedRoles.pair.push(...group.roles.filter(role => role.pair === +key));
                 }
             });
             return group;
         }
 
         function findIcon(pair: number) {
-            return pairIconList[pair];
+            return pairIconList[pair as unknown as keyof typeof pairIconList];
         }
-
         function setBGC(label: string) {
             return colorList.find(x => x.label === label)?.color || 'fff';
         }
-        function generateStyle(item: Role){
+        function generateStyle(item: Role) {
             return {
                 'background-image': `url(./${item.src})`,
                 'width': `${Math.round(props.cardWidth)}px`
             };
         };
-        return { RoleArray, findIcon ,generateStyle };
+        return { RoleArray, findIcon, generateStyle, pairRoles, singleRoles };
     }
 })
 </script>
+
 <style scoped lang="scss">
 $red: $info-red;
 $blue: $info-blue;
 $grey: $info-grey;
 $shadow: 4px 4px 4px 0 rgba(0, 0, 0, 0.25);
-$lg-full-height:calc((100vh - (48px * 4) - 100px) / 2);
-$md-full-height:calc((100vh - (12px * 4) - 100px) / 2);
+$lg-full-height: calc((100vh - (48px * 4) - 100px) / 2);
+$md-full-height: calc((100vh - (12px * 4) - 100px) / 2);
+
 .bi {
     &.red {
         color: $red;
@@ -113,29 +121,37 @@ $md-full-height:calc((100vh - (12px * 4) - 100px) / 2);
         color: $grey;
     }
 }
+
 .home {
     width: 100vw;
     height: 100vh;
 }
-.card-full-height{
+
+.card-full-height {
     height: $lg-full-height;
+
     @media screen and (max-width: 1280px) {
         height: $md-full-height;
     }
 }
-.card-md-height{
+
+.card-md-height {
     height: calc($lg-full-height - 20px);
+
     @media screen and (max-width: 1280px) {
         height: calc($md-full-height - 20px);
     }
 }
-.card-padding{
+
+.card-padding {
     padding: 24px 16px 0px 16px;
+
     @media screen and (max-width: 1280px) {
         padding: 32px 16px 0px 16px;
     }
 }
-.card-wrap {  
+
+.card-wrap {
     border-radius: 8px;
     box-shadow: $shadow;
     border-style: solid;
@@ -143,7 +159,7 @@ $md-full-height:calc((100vh - (12px * 4) - 100px) / 2);
     border-top-width: 1px;
     border-right-width: 1px;
     border-bottom-width: 1px;
-   
+
     &.red {
         border-color: $red;
     }
@@ -156,10 +172,12 @@ $md-full-height:calc((100vh - (12px * 4) - 100px) / 2);
         border-color: $grey;
     }
 }
+
 .card-background {
     background-size: cover;
     background-position: center;
 }
+
 .team-wrap>.card-wrap:first-child {
     border-left-width: 1px;
 }
@@ -198,6 +216,5 @@ $md-full-height:calc((100vh - (12px * 4) - 100px) / 2);
 .ver-text {
     -webkit-writing-mode: vertical-lr;
     writing-mode: vertical-lr;
-}
-</style>
+}</style>
 
