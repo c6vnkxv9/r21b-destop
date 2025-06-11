4781d9e0b2b22a50151198b7a6567f74ba25b076
Kaamii <kami.yh@medialand.tw>
2025-06-11 12:22:54 +0800

    feat: 新增部署腳本和更新 Vue 配置以複製 README 文件至 dist 目錄

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
+const fs = require('fs')
+const path = require('path')
+
 module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === "production" ? "https://kamiiliu.github.io/r21b-destop/" : "/",
   transpileDependencies: true,
@@ -10,4 +13,14 @@ module.exports = defineConfig({
       },
     },
   },
+  chainWebpack: config => {
+    config.plugin('copy').tap(options => {
+      options[0].patterns.push({
+        from: path.resolve(__dirname, 'public/gh-pages-readme.md'),
+        to: path.resolve(__dirname, 'dist/README.md'),
+        toType: 'file',
+      })
+      return options
+    })
+  }
 })
